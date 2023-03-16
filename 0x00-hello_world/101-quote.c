59798b194a5075416820cd28f3e15488eced87a7
git <git@github.com>
2023-03-16 12:56:46 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Printing a statement without printf() or puts()
+ * Return: 1
+ */
+
+int main(void)
+{
+	system("echo and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
