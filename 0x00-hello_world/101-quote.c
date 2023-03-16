23d92de3cc990e02fc516a86ce0a5a6d46e22b67
git <git@github.com>
2023-03-16 13:07:23 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,13 @@
 /**
  * main - Entry Point
  * Description: Printing a statement without printf() or puts()
+ *
  * Return: 1
  */
 
 int main(void)
 {
 	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(1, message, sizeof(message));
+	write(1, message, strlen(message));
 	return (1);
 }
