0a13a20a7067c6312ac190ae01e6fdc5b524e5b9
git <git@github.com>
2023-03-17 13:22:30 -0700

    Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,13 @@ int main(void)
 	char character = 'a';
 	int number = 'z' - 'a';
 	int i;
-	for(i = 0; i <= number; i++)
+
+	for (i = 0; i <= number; i++)
 	{
 		putchar(character++);
 	}
 	character = 'A';
-	for(i = 0; i <= number; i++)
+	for (i = 0; i <= number; i++)
 	{
 		putchar(character++);
 	}
