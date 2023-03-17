073df1a2401911dab6ee91e24bc35681dcaf2497
git <git@github.com>
2023-03-17 07:05:51 -0700

    PosOrNEg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,8 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/* main - Entry Point
+/* 
+ * main - Entry Point
  * main - Entry Point
  * Description - If else
  *
@@ -19,15 +20,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
