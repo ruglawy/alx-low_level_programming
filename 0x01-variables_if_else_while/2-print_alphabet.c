c9ed9ddb45e4aa9c698f11d5cd32322b1369d38c
git <git@github.com>
2023-03-17 07:30:02 -0700

    PrintAlphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Idk
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char character = 'a';
+	int ascii = 'z' - 'a';
+	int i;
+
+	for (i = 1; i <= ascii; i++)
+	{
+		putchar(character);
+	}
+	putchar('\n');
+
+	return (0);
+}
