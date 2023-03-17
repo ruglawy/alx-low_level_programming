daa90b0cceace02f5de1b06e7efcc8070321a947
git <git@github.com>
2023-03-17 07:24:20 -0700

    LastDigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,17 +14,18 @@ int main(void)
 {
 	int n;
 	int last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	printf("Last digit of %d is %d", n, last_digit);
 	if (last_digit > 5)
 	{
-		printf("and is greater than 5\n");
+		printf(" and is greater than 5\n");
 	}
 	else if (last_digit == 0)
 	{
-		printf("and is 0\n");
+		printf(" and is 0\n");
 	}
 	else
 	{
