88a3941a27c21a55020356979ed2783aa787b996
git <git@github.com>
2023-03-21 15:05:58 -0700

    TimesTable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,14 @@
 void times_table(void)
 {
 	int i, j;
-
+	
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			int unit = (i * j) % 10;
 			int tenth = (i * j) / 10;
+
 			if (tenth != 0)
 			{
 				_putchar(tenth + '0');
