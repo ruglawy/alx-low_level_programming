83f7188f9ab99d55d620c7229ac41014780d2707
git <git@github.com>
2023-03-21 13:17:47 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Returns absolute value
+ * Description: kk
+ *
+ * Return: value after absolute process
+ */
+
+int _abs(int number)
+{
+	if (number < 0)
+	{
+		return (number * -1);
+	}
+	return (number);
+}
