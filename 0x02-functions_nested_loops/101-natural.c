22bb3a6fd2f41921786bc46426e3e6f689d01216
git <git@github.com>
2023-03-21 16:03:57 -0700

    Natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: ..
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+	
+	for (i = 1; i < 1024; i++)
+	{
+		if (!(i % 3))
+		{
+			sum += i;
+		}
+		else if (!(i % 5))
+		{
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+
+	return (0);
+}
