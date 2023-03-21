47e701ac8a030734efede6899b176bfe2d0c5592
git <git@github.com>
2023-03-21 13:22:54 -0700

    PrintLastDigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit: prints last digit of a number
+ * Description: stated
+ * @number: number
+ *
+ * Return: last digit of number
+ */
+
+int print_last_digit(int number)
+{
+	_putchar((number % 10) - '0');
+	return (number % 10);
+}
