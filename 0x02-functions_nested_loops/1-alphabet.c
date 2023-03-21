f522c657e282dbbf93b34cdbf14e141235c837a2
git <git@github.com>
2023-03-21 12:23:13 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <main.h>
+/**
+ * print_alphabet -  prints alphabet
+ * main - Entry Point
+ * Description: idk
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char character = 'a';
+	int i;
+	int number = 'a' - 'z';
+
+	for (i = 0; i <= number; i++)
+	{
+		_putchar(character);
+	}
+	_putchar('\n');
+
+	return (0);
+
+}
