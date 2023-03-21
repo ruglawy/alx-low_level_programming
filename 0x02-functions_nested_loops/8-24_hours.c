e9c78192a155c7d6af8525b7eecf06267039721f
git <git@github.com>
2023-03-21 14:10:21 -0700

    JackBauerMinutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,13 +11,14 @@ void jack_bauer(void)
 
 	for (hours = 0; hours < 24; hours++)
 	{
-		for (minutes = 0; minutes < 60; minutes ++)
+		for (minutes = 0; minutes < 60; minutes++)
 		{
 			_putchar((hours / 10) + '0');
 			_putchar((hours % 10) + '0');
 			_putchar(':');
 			_putchar((minutes / 10) + '0');
 			_putchar((minutes % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
