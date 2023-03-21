75be5b2c92e4b4bf6b41353154ae26e25ae5138d
git <git@github.com>
2023-03-21 12:42:40 -0700

    Alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints Alphabet 10 times
+ * Description: Nothing specific
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char character = 'a';
+	int number  = 'z' - 'a';
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= number; j++)
+		{
+			_putchar(character++);
+		}
+		_putchar('\n');
+	}
+}
