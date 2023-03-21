6e9638ebaa2d4548e5a44cc684f633d0332aad19
git <git@github.com>
2023-03-21 12:58:12 -0700

    isLower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * is_lower - returns if character is lowercase
+ * Description: ...
+ *
+ * Return: 0 if uppercase, 1 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
