2ea7bbc90cbeaa14a9fc892bc175b1633f700536
git <git@github.com>
2023-03-21 14:09:15 -0700

    JackBauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - Print minutes and hours
+ * Description: ..
+ *
+ */
+
+void jack_bauer(void)
+{
+	int minutes, hours;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes ++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+		}
+	}
+}
