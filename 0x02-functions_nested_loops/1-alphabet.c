b2f7bd8ce9e9b2ad2f296e91efe37f9ca03468ce
git <git@github.com>
2023-03-21 12:28:33 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * main - Entry Point
  * Description: idk
  *
- * 
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
@@ -19,4 +19,9 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
