1aa71112fe185d56589f2cc018fc7ea12ae4b390
git <git@github.com>
2023-03-21 12:37:13 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,12 +11,12 @@ void print_alphabet(void)
 {
 	char character = 'a';
 	int i;
-	int number = 'a' - 'z';
+	int number = 'z' - 'a';
 
 	for (i = 1; i <= number; i++)
 	{
-		_putchar(character);
+		_putchar(character++);
 	}
-	/*_putchar('\n');*/
+	_putchar('\n');
 
 }
