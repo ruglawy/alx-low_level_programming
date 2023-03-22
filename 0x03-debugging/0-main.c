92b5acef386c3e1ad998465b9bdacd864ef27b46
git <git@github.com>
2023-03-22 08:11:05 -0700

    TestPositiveorNegative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry Point
+ * Description: ...
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
