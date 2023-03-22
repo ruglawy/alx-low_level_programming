e69b187d9e8de97c87b0c4e07123a1c3cec8b03f
git <git@github.com>
2023-03-22 08:30:48 -0700

    LargestNumber

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * largest_number - Prints largest number
+ * @a: first number
+ * @b: second number
+ * @c: third number
+ *
+ * Return: Largest number
+ */
+int largest_number(int a, int b, int c);
