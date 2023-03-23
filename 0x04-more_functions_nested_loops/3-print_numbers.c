a8deaf1bc04e0ba9a7166c26d4dddaaa862ffc63
git <git@github.com>
2023-03-23 10:29:40 -0700

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char character = '0';
+	int i;
+
+	for (i = 0; i <= '9'; i++)
+	{
+		_putchar(character++);
+	}
+	_putchar('\n');
+}
