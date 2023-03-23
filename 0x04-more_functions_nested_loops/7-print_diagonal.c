b0c700417990eff0220840b30a5e57e3786a6aa2
git <git@github.com>
2023-03-23 11:41:36 -0700

    PrintDiagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagonal - Prints diagonal
+ * @n: number of diagonal lines
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+	
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
