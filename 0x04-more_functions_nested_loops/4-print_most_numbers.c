7817b2c296065b8087e39249f8c365ea62524509
git <git@github.com>
2023-03-23 10:48:20 -0700

    PrintMostNumbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints all numbers except 2, 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char character = '0';
+	int range = '9' - '0';
+	int i;
+
+	for (i = 0; i <= range; i++)
+	{
+		if (!(character == '2' || character == '4'))
+		{
+			_putchar(character++);
+		}
+	}
+	_putchar('\n');
+}
