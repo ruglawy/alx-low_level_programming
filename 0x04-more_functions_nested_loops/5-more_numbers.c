9785a8f17f0e0f3b95705257ef629d9496015bbd
git <git@github.com>
2023-03-23 11:16:37 -0700

    moreNumbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdbool.h>
 /**
  * more_numbers - prints numbers from 0 to 14
  * Return: void
@@ -9,25 +8,25 @@ void more_numbers(void)
 	char character = '0';
 	int range = '9' - '0';
 	int i, j;
-	bool flag = false;
+	int flag = 0;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (flag)
+			if (flag == 1)
 			{
 				_putchar('1');
 			}
 			_putchar(character++);
 			if (character > '9')
 			{
-				flag = true;
+				flag = 1;
 				character = '0';
 			}	
 		}
 		character = '0';
-		flag = false;
+		flag = 0;
 		_putchar('\n');	
 	}
 }
