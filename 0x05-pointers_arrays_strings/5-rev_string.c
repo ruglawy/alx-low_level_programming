5670a411d1764d9f3e2d2da646d53aba3b5f2fff
git <git@github.com>
2023-03-27 13:16:06 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int i;
+	char *str = &s[0];
+
+	while (*str != '\0')
+	{
+		str++;
+		length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}
