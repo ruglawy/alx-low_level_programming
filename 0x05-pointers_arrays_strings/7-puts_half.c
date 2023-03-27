955928b6babd16d55978ffe354a84ae306acbe24
git <git@github.com>
2023-03-27 13:26:16 -0700

    PutsHalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * puts_half - prints half of the string
+ * @str: string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+	char *s = &s[0];
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+
+	if (length % 2 == 1)
+	{
+		for (i = length / 2; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (length - 1) / 2; i < length, i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+
+}
