a306a6507207db2d4511a74068ce74f143c557be
git <git@github.com>
2023-03-27 11:53:07 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - printd a string to stdout
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str++);
+	}
+}
