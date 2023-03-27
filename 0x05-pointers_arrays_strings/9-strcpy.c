67412ae31616fe0ee5c055706af264dbe0f45362
git <git@github.com>
2023-03-27 13:47:02 -0700

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copies string to string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer of dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+
+	return dest;
+}
