03d570c262a5905eb72ba1163da2a7d7261a37e2
git <git@github.com>
2023-03-29 13:46:07 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,14 +9,29 @@ char *leet(char *s)
 {
 	int count = 0;
 
-	while (*(s + count) != '\0')
+	while (*(s + count) != '\0)
 	{
-		if (1)
+		if (*(s + count) == 'a' || *(s + count) == 'A')
 		{
+			*(s + count) = 4 + '0';
 		}
-		else if (1)
+		else if (*(s + count) == 'e' || *(s + count) == 'E')
 		{
+			*(s + count) = 3 + '0';
+		}
+		else if (*(s + count) == 'o' || *(s + count) == 'O')
+		{
+			*(s + count) = 0 + '0'
+		}
+		else if (*(s + count) == 't' || *(s + count) == 'T')
+		{
+			*(s + count) = 7 + '0';
+		}
+		else if (*(s + count) == 'l' || *(s + count) == 'L')
+		{
+			*(s + count) = 1 + '0';
 		}
 	}
+
 	return (s);
 }
