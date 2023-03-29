304b0b5efe9a88037845f6bc102b9f1613b03e30
git <git@github.com>
2023-03-29 13:35:20 -0700

    Test

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - encodes string
+ * @s: string
+ *
+ * Return: string
+ */
+char *leet(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if (1)
+		{
+		}
+		else if (1)
+		{
+		}
+	}
+	return (s);
+}
