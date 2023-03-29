b34d6e2ec5d6a6a7bf57065e2c40b85207833b78
git <git@github.com>
2023-03-29 12:31:06 -0700

    Strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,25 @@ int _strcmp(char *s1, char *s2)
 	int countS1 = 0;
 	int countS2 = 0;
 
-	while (*(s1 + countS1) != '\0')
+	while (*(s1 + countS1) != '\0' && *(s2 + countS2) != '\0')
 	{
+		if (*(s1 + countS1) != *(s2 + countS2))
+		{
+			return (*(s1 + countS1) - *(s2 + countS2));
+		}
 		countS1++;
-	}
-
-	while (*(s2 + countS2) != '\0')
-	{
 		countS2++;
 	}
 
-	return (countS1 - countS2);
+	if (*(s1 + countS1) != '\0')
+	{
+		return (*(s1 + countS1));
+	}
+
+	if(*(s2 + countS2) != '\0')
+	{
+		return (*(s2 + countS2) * -1);
+	}
+
+	return (0);
 }
