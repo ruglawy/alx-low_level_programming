3852716c72ec517c8ac78a1ce1012bfd07bc0a99
git <git@github.com>
2023-03-29 14:05:50 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,31 +8,22 @@
 char *leet(char *s)
 {
 	int count = 0;
+	int i;
+	char lowerCase[] = {'a', 'e', 'o', 't', 'l'};
+	char upperCase[] = {'A', 'E', 'O', 'T', 'L'};
+	char numbers[] = {'4', '3', '0', '7', '1'};
 
 	while (*(s + count) != '\0')
 	{
-		if (*(s + count) == 'a' || *(s + count) == 'A')
+		for (i = 0; i < 5; i++)
 		{
-			*(s + count) = 4 + '0';
-		}
-		else if (*(s + count) == 'e' || *(s + count) == 'E')
-		{
-			*(s + count) = 3 + '0';
-		}
-		else if (*(s + count) == 'o' || *(s + count) == 'O')
-		{
-			*(s + count) = 0 + '0';
-		}
-		else if (*(s + count) == 't' || *(s + count) == 'T')
-		{
-			*(s + count) = 7 + '0';
-		}
-		else if (*(s + count) == 'l' || *(s + count) == 'L')
-		{
-			*(s + count) = 1 + '0';
+			if (*(s + count) == lowerCase[i] || *(s + count) == upperCase[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
 		}
 		count++;
 	}
-
 	return (s);
 }
