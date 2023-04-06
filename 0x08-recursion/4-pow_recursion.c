2a3a0ecc5cf290787fb9fec3e842936e205f17f4
git <git@github.com>
2023-04-05 17:58:51 -0700

    PowRecursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - msh lazem a2ool
+ * @x: number
+ * @y: number
+ *
+ * Return: result
+ */
+int _pow_recursion(int x, int y);
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x * y);
+	}
+	return (x * _pow_recursion(x, y - 1));
+
+}
