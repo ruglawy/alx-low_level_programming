6f1ff852dd3dc21edce5db879549e765f4783d2e
git <git@github.com>
2023-04-10 19:17:48 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - ...
+ * @argc: ...
+ * @argv: ...
+ *
+ * Return: ...
+ */
+int main(int argc __attribute__((unused)), char* argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
