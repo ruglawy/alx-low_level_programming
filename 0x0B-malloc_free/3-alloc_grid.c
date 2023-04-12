cd10d827820cabb72e884c5826b86765a04cdcb3
git <git@github.com>
2023-04-11 19:18:15 -0700

    AllocGrid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - ...
+ * @width: ...
+ * @height: ...
+ *
+ * Return: ...
+ */
+int **alloc_grid(int width, int height)
+{
+	int** arr = (int**)malloc(width * sizeof(int*));
+	int i, j;
+
+	for (i = 0; i < width; i++)
+	{
+		arr[i] = (int*)malloc(height * sizeof(int));
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
