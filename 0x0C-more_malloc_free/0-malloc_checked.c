54e12df816643849ae87bf52cb7cceddb877aba5
git <git@github.com>
2023-04-13 11:26:50 -0700

    MallocCheck

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - ...
+ * @b: ...
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p = malloc(b);
+	
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+}
