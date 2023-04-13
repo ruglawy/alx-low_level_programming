05c015f4044802f69296a3caed97c08660d4ed75
git <git@github.com>
2023-04-13 12:15:44 -0700

    ArrayRange

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -24,7 +24,7 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < sizeof(int) * (max - min + 1); i++)
+	for (i = 0; i < max - min + 1; i++)
 	{
 		arr[i] = min + i;
 	}
