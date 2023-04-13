51f4ca7a1e0abe90cbbf75abe38a06b6808e3c34
git <git@github.com>
2023-04-13 11:36:04 -0700

    StringConcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings with n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: pointer to new string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int count1 = 0;
+	int count2 = 0;
+	char *newString;
+	int i;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	while (*(s1 + count1) != '\0')
+	{
+		count1++;
+	}
+	while (*(s2 + count2) != '\0' && count2 < n)
+	{
+		count2++;
+	}
+
+	newString = malloc(sizeof(char) * ((count1 + count2) + 1));
+
+	for (i = 0; i < count1; i++)
+	{
+		*(newString + i) = *(s1 + i);
+	}
+	for (i = 0; i < count2; i++)
+	{
+		*(newString + i + count1) = *(s2 + i);
+	}
+
+	return (newString);
+}
