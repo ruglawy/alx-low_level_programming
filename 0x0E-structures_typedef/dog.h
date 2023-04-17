526c4d845dfcca07665ae005e68fe6c09e35a3c6
git <git@github.com>
2023-04-17 13:25:47 -0700

    DogStruct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * dog: struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: ..
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
