7941e9e7e004538a42669f1aa3b94234f7c9b33f
git <git@github.com>
2023-04-17 14:45:26 -0700

    FreeDog'

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_dog - frees dogs!
+ * @d: dog to be freed
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
