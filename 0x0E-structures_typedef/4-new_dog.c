4a6758cbf7eebd6a4e34cd599a96886cdf858210
git <git@github.com>
2023-04-17 14:42:40 -0700

    NewDog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,12 +18,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (d == NULL)
 	{
+		free(d);
 		return (NULL);
 	}
 	d->name = malloc(sizeof(char) * (strlen(name) + 1));
 	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (d->name == NULL || d->owner == NULL)
 	{
+		free(d->name);
+		free(d->owner);
+		free(d);
 		return (NULL);
 	}
 	strcpy(d->name, name);
