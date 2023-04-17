d6015403f3b27c6d203f70c486f26af674cfc70b
git <git@github.com>
2023-04-17 14:37:24 -0700

    NewDog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - creates an instance of struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: instance of struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	d->name = malloc(sizeof(char) * (strlen(name) + 1));
+	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (d->name == NULL || d->owner == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(d->name, name);
+	strcpy(d->owner, owner);
+
+	return (d);
+}
