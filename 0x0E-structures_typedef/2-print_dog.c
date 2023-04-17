157ef1d39480ae67914e3633b379639f74389ff4
git <git@github.com>
2023-04-17 14:29:01 -0700

    PrintDog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,26 +14,26 @@ void print_dog(struct dog *d)
 	}
 	if (d->name != NULL)
 	{
-		printf("Name: %s", d->name);
+		printf("Name: %s\n", d->name);
 	}
 	else
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	}
 	if (d->age != 0)
 	{
-		printf("Age: %f", d->age);
+		printf("Age: %f\n", d->age);
 	}
 	else
 	{
-		printf("Age: (nil)");
+		printf("Age: (nil)\n");
 	}
 	if (d->owner != NULL)
 	{
-		printf("Owner: %s", d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 	else
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 }
