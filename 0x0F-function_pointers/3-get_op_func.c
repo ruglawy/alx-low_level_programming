b03772e72a2423f4b1c7b081c4dbcbe7fd9e5f19
git <git@github.com>
2023-04-19 16:11:18 -0700

    CalcProgram

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,7 +13,7 @@ int (*get_op_func(char *s))(int, int)
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
-		{"'*'", op_mul},
+		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
@@ -23,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (s[0] == ops[i].op || s[1] == ops[i].op)
+		if (s[0] == ops[i].op[0] || s[1] == ops[i].op[0])
 		{
 			return (ops[i].f);
 		}
