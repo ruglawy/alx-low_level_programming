259dd786d3459cc0dc1d6a2e6a7e78bca39562c3
git <git@github.com>
2023-04-29 14:52:29 -0700

    AddNodeIndex

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		traverse = traverse->next;
 		i++;
 	}
+	if (traverse == NULL)
+	{
+		return (NULL);
+	}
 	new->next = traverse->next;
 	traverse->next = new;
 	return (new);
