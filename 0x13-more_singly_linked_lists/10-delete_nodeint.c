36531f48b9765e8de684a632ddc58accb79a6fb9
git <git@github.com>
2023-04-29 15:30:41 -0700

    DeleteNodeIndex

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,6 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (traverse != NULL && i < index - 1)
 	{
 		traverse = traverse->next;
+		i++;
 	}
 	ptr = traverse->next;
 	traverse->next = ptr->next;
