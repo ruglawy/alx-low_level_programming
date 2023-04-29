7f828bf6d50c808e6f82afcff7271ebad736ab75
git <git@github.com>
2023-04-29 14:46:13 -0700

    AddNodeIndex

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * insert_nodeint_at_index - inserts node at a specific index
+ * @head: list
+ * @idx: index
+ * @n: int variable
+ *
+ * Return: address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *traverse, *new;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	traverse = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	while (traverse != NULL && i < index)
+	{
+		traverse = traverse->next;
+	}
+	if (traverse == NULL)
+	{
+		return (NULL);
+	}
+	new->next = traverse->next;
+	traverse->next = new;
+	return (new);
+
+}
