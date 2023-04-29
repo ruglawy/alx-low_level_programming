84533f4b897d6d6e3de9d6fd4551f3f2dd58808c
git <git@github.com>
2023-04-29 14:10:47 -0700

    AddNodeEnd

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,6 +20,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new->n = n;
 	traverse = *head;
+	if (traverse == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
 
 	while (traverse->next != NULL)
 	{
