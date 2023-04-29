0e16acf66dc40907b09c48b8fb06302be8a6399c
git <git@github.com>
2023-04-29 14:33:45 -0700

    NodeAtIndex

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * get_nodeint_at_index - returns nth node of linked list
+ * @head: list
+ * @index: index
+ *
+ * Return: node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr;
+	unsigned int i = 0;
+
+	ptr = head;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (i < index && ptr != NULL)
+	{
+		ptr = ptr->next;
+	}
+	return (ptr);
+}
