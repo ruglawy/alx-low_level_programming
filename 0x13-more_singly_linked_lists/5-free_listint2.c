7b6dd10e40703784f742ebcb9ea6ebcc82126c07
git <git@github.com>
2023-04-29 14:17:49 -0700

    FreeListNULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_listint2 - frees a linked list and sets head to NULL
+ * @head: linked list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr, *traverse;
+
+	traverse = *head;
+	while (traverse != NULL)
+	{
+		ptr = traverse;
+		traverse = traverse->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
