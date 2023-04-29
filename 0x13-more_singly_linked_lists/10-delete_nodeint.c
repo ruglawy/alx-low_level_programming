10133c8ec5049292e37af102efa87bd5360f6696
git <git@github.com>
2023-04-29 15:29:32 -0700

    DeleteNodeIndex

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * delete_nodeint_at_index - deletes node at a specific index
+ * @head: list
+ * @index: index
+ *
+ * Return: 1 if success, -1 if failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *traverse, *ptr;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	traverse = *head;
+	while (traverse != NULL)
+	{
+		traverse = traverse->next;
+		i++;
+	}
+	if (index > i)
+	{
+		return (-1);
+	}
+	i = 0;
+	traverse = *head;
+	while (traverse != NULL && i < index - 1)
+	{
+		traverse = traverse->next;
+	}
+	ptr = traverse->next;
+	traverse->next = ptr->next;
+	free(ptr);
+	return (1);
+}
