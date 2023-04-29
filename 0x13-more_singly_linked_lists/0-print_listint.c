90d98c036c31194865998bfd93f33761ca2883c0
git <git@github.com>
2023-04-29 13:58:14 -0700

    PrintListInt

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - prints linked list
+ * @h: list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		number++;
+		h = h->next;
+	}
+	return (number);
+}
