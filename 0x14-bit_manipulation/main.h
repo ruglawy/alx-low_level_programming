02b1077955d8159493f973cef0fff463934a9021
git <git@github.com>
2023-05-04 14:28:29 -0700

    GetBit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,4 +12,5 @@ int _putchar(char c);
 unsigned int power(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
