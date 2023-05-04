ec2e0fcd249bc08b116c16fdc3cf36f0e2beb724
git <git@github.com>
2023-05-04 14:10:50 -0700

    PrintBinary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,7 +10,6 @@
  */
 int _putchar(char c);
 unsigned int power(int x, int y);
-unsigned int po(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 #endif
