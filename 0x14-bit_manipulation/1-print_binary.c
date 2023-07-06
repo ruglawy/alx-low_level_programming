c55ebaffb446d7e7c71a48706716738a2f5384f7
ruglawy <git@git.com>
2023-07-06 02:11:10 -0700

    printBinary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,19 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * power - power
- * @x: x
- * @y: y
- *
- * Return: power
- */
-unsigned int power(int x, int y)
-{
-	if (y == 0)
-		return (1);
-	return (x * power(x, y - 1));
-}
-/**
  * print_binary - converts int to binary and prints it
  * @n: number
  *
