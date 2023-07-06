0724a32ca13b1049cc8df7bb2d703ba3715a5ea4
ruglawy <git@git.com>
2023-07-06 01:39:16 -0700

    BinaryToUint

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * _putchar - prints characters to screen
+ * @c: character
+ *
+ * Return: int
+ */
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+#endif
