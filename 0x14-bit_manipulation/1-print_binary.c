edecae85cd8f0b172b11539bb5c2f9c5d5c8f4b7
Ruglawy <git@github.com>
2023-08-03 14:44:06 -0700

    Print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * power - power
+ * @x: x
+ * @y: y
+ *
+ * Return: power
+ */
+unsigned int power(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	return (x * power(x, y - 1));
+}
+/**
+ * print_binary - converts int to binary and prints it.
+ * @n: number
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+	int p = 0;
+
+	if (n == 0)
+	{
+		printf("%d", 0);
+		return;
+	}
+	while (1)
+	{
+		if (power(2, p) > n)
+		{
+			p--;
+			break;
+		}
+		p++;
+	}
+	for (i = p; i >= 0; i--)
+	{
+		if (power(2, i) <= n)
+		{
+			printf("%d", 1);
+			n -= power(2, i);
+		}
+		else
+		{
+			printf("%d", 0);
+		}
+	}
+}
