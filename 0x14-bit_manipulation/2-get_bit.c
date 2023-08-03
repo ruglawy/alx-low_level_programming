1042bf68d43f632e61690f58f7460cd51ccc8fe5
Ruglawy <git@github.com>
2023-08-03 14:58:02 -0700

    Getbit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * get_bit - gets bit at given index
+ * @n: number
+ * @index: index
+ *
+ * Return: bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+	{
+		return (0);
+	}
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
+}
