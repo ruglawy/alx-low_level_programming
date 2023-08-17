8235d80755c0befeb1f5031fd0c9d9b4a98e74f4
Ruglawy <git@github.com>
2023-08-17 16:59:31 -0700

    PrintDlist

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of dlistint_t list
+ * @h: list
+ *
+ * Return: number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
