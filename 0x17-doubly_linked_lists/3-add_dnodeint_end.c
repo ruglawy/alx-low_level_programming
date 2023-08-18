1c8ce244798d6f1408c24be9c355dfc91d2d9914
Ruglawy <git@github.com>
2023-08-17 17:35:15 -0700

    AddNodeEnd

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds node at the end
+ * @head: list
+ * @n: el
+ *
+ * Return: address
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *h;
+
+	h = *head;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	while (h->next != NULL)
+	{
+		h = h->next;
+	}
+	new->n = n;
+	new->prev = h;
+	new->next = NULL;
+	h->next = new;
+
+	return (new);
+}
