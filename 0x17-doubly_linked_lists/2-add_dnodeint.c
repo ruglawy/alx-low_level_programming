f9bbb31328502cf734c2c93a9a804c538c4bf47c
Ruglawy <git@github.com>
2023-08-17 17:22:42 -0700

    Addnoddde

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,10 +20,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new;
-		new->next = NULL;
+		*head->next = NULL;
+		*head->prev = NULL;
 		return (new);
 	}
 	new->next = *head;
+	*head->prev = new;
 	*head = new;
 
 	return (new);
