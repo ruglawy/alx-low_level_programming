6dd1f90fd472ca57f42595da5922aab83939fbbc
Ruglawy <git@github.com>
2023-08-17 17:14:30 -0700

    AddNode

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 	new->next = *head;
 	*head = new;
 
