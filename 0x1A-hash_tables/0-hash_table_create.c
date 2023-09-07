89292af0484d3cefe822b2c343f1d5d3b26a3241
Ruglawy <git@github.com>
2023-09-07 13:43:05 -0700

    Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **array;
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
+	newtable = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
