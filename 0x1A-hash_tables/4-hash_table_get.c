ac8c8675c07cca874e806c3c56994b6d510137f9
Ruglawy <git@github.com>
2023-09-07 06:19:48 -0700

    getElement

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get hash table
+ *
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: element
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+	if (key == NULL || *key == '\0')
+	{
+		return (NULL);
+	}
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
+	}
+	return (NULL);
+}
